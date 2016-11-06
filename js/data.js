f50735187b8b9c6c78bb66c9ff4bdd8f3d62cadd
Ana-Maria29 <anamaria.apostoaiei@gmail.com>
2016-11-06 11:51:19 +0100

    Schedule part

---
+++ js/data.js
@@ -0,0 +1,6 @@
+var TinderboxEvents = {
+	'TODAY' : '<span>Today</span>',
+	'06-22-2017' : '<span id="container-date"><span class="left-text-span">23th</span><span class="text-right-span"><ul class="list-group"><li class="list-group-item date">9:00 Meeting time</li><li class="list-group-item date">9:00 Meeting time</li><li class="list-group-item date">9:00 Meeting time</li></ul></span></span>',
+	'06-23-2017' : '<span id="container-date"><span class="left-text-span">23th</span><span class="text-right-span"><ul class="list-group"><li class="list-group-item date">9:00 Meeting time</li><li class="list-group-item date">9:00 Meeting time</li><li class="list-group-item date">9:00 Meeting time</li></ul></span></span>',
+	'06-24-2017' : '<span id="container-date"><span class="left-text-span">23th</span><span class="text-right-span"><ul class="list-group"><li class="list-group-item date">9:00 Meeting time</li><li class="list-group-item date">9:00 Meeting time</li><li class="list-group-item date">9:00 Meeting time</li></ul></span></span>',
+};
--- js/main.js
+++ js/main.js
@@ -62,4 +62,76 @@ $(function () {
         }
         init();
     });
+    //calendario
+    
+             $(document).on('shown.calendar.calendario', function(e, instance){
+                    if(!instance) instance = cal;
+                    var $cell = instance.getCell(new Date().getDate());
+                    if($cell.hasClass('fc-today')) $cell.trigger('click.calendario');
+                });
+            
+                var transEndEventNames = {
+                        'WebkitTransition' : 'webkitTransitionEnd',
+                        'MozTransition' : 'transitionend',
+                        'OTransition' : 'oTransitionEnd',
+                        'msTransition' : 'MSTransitionEnd',
+                        'transition' : 'transitionend'
+                    },
+                    transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
+                    $wrapper = $( '#custom-inner' ),
+                    $calendar = $( '#calendar' ),
+                    cal = $calendar.calendario({
+                        onDayClick : function( $el, data, dateProperties ) {
+
+                            if(data.content.length > 0 ) {
+                                showEvents(data.content, dateProperties );
+                            }
+
+                        },
+                        caldata : TinderboxEvents,
+                        displayWeekAbbr : true,
+                        events: 'click'
+                    } ),
+                    $month = $( '#custom-month' ).html( cal.getMonthName() ),
+                    $year = $( '#custom-year' ).html( cal.getYear() );
+
+                $( '#custom-next' ).on( 'click', function() {
+                    cal.gotoNextMonth( updateMonthYear );
+                } );
+                $( '#custom-prev' ).on( 'click', function() {
+                    cal.gotoPreviousMonth( updateMonthYear );
+                } );
+
+                function updateMonthYear() {                
+                    $month.html( cal.getMonthName() );
+                    $year.html( cal.getYear() );
+                }
+
+                // just an example..
+                function showEvents( contentEl, dateProperties ) {
+
+                    hideEvents();
+                    /* <h4>Events for ' + dateProperties.monthname + ' ' + dateProperties.day + ', ' + dateProperties.year + '</h4> */
+                    var $events = $( '<div id="custom-content-reveal" class="custom-content-reveal"></div>' ),
+                        $close = $( '<span class="custom-content-close"></span>' ).on( 'click', hideEvents );
+
+                    $events.append( contentEl.join('') , $close ).insertAfter( $wrapper );
+                    
+                    setTimeout( function() {
+                        $events.css( 'top', '0%' );
+                    }, 25 );
+
+                }
+                function hideEvents() {
+
+                    var $events = $( '#custom-content-reveal' );
+                    if( $events.length > 0 ) {
+                        
+                        $events.css( 'top', '100%' );
+                        Modernizr.csstransitions ? $events.on( transEndEventName, function() { $( this ).remove(); } ) : $events.remove();
+
+                    }
+
+                }
+            
 });
