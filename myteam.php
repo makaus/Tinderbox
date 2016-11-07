b1808b23d855c97be986110668a2e415f76e4ba8
Casper Storm <casper.storm@hotmail.com>
2016-11-07 17:43:31 +0100

    Made the dashboard and my team page.
    Made the dashboard and my team page.

---
+++ myteam.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Tinderbox</title>
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/mystyle.css">
+</head>
+<body>
+	<div class="background">
+    	<img src="img/background-schedule.png" alt="background-sign-in">
+  	</div></div>
+	<div class="container myteam">
+		<div class="row">
+		<div class="col-xs-12 menu-container">
+			<div class="menu">
+				<div class="col-xs-2">
+					<div class="table">
+						<div class="cell">
+							<a href="dashboard.php"><img src="img/home-button.png" alt="" class="img-responsive center-block"></a>
+						</div>
+					</div>
+				</div>
+				<div class="col-xs-8 text-center">
+					<div class="table">
+						<div class="cell">
+							<h1>My team</h1>
+						</div>
+					</div>
+				</div>
+				<div class="col-xs-2">
+					<div class="table">
+						<div class="cell">
+							<img src="img/search.png" alt="" class="img-responsive center-block">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-xs-4">
+			<div class="team-member">
+				<img src="img/member1.jpg" alt="Member" class="img-responsive center-block">
+				<h2>Lara Andersen</h2>
+				<h3>Volunteer</h3>
+			</div>
+			<div class="team-member">
+				<img src="img/member1.jpg" alt="Member" class="img-responsive center-block">
+				<h2>Lara Andersen</h2>
+				<h3>Volunteer</h3>
+			</div>
+			<div class="team-member">
+				<img src="img/member1.jpg" alt="Member" class="img-responsive center-block">
+				<h2>Lara Andersen</h2>
+				<h3>Volunteer</h3>
+			</div>
+			<div class="team-member">
+				<img src="img/member1.jpg" alt="Member" class="img-responsive center-block">
+				<h2>Lara Andersen</h2>
+				<h3>Volunteer</h3>
+			</div>
+		</div>
+		<div class="col-xs-4">
+			<div class="team-member">
+				<img src="img/member2.jpg" alt="Member" class="img-responsive center-block">
+				<h2>Jonna Olsen</h2>
+				<h3>Volunteer</h3>
+			</div>
+			<div class="team-member">
+				<img src="img/member2.jpg" alt="Member" class="img-responsive center-block">
+				<h2>Jonna Olsen</h2>
+				<h3>Volunteer</h3>
+			</div>
+			<div class="team-member">
+				<img src="img/member2.jpg" alt="Member" class="img-responsive center-block">
+				<h2>Jonna Olsen</h2>
+				<h3>Volunteer</h3>
+			</div>
+			<div class="team-member">
+				<img src="img/member2.jpg" alt="Member" class="img-responsive center-block">
+				<h2>Jonna Olsen</h2>
+				<h3>Volunteer</h3>
+			</div>
+		</div>
+		<div class="col-xs-4">
+			<div class="team-member">
+				<img src="img/member3.jpg" alt="Member" class="img-responsive center-block">
+				<h2>Lone Larsen</h2>
+				<h3>Volunteer</h3>
+			</div>
+			<div class="team-member">
+				<img src="img/member3.jpg" alt="Member" class="img-responsive center-block">
+				<h2>Lone Larsen</h2>
+				<h3>Volunteer</h3>
+			</div>
+			<div class="team-member">
+				<img src="img/member3.jpg" alt="Member" class="img-responsive center-block">
+				<h2>Lone Larsen</h2>
+				<h3>Volunteer</h3>
+			</div>
+			<div class="team-member">
+				<img src="img/member3.jpg" alt="Member" class="img-responsive center-block">
+				<h2>Lone Larsen</h2>
+				<h3>Volunteer</h3>
+			</div>
+		</div>
+		</div>
+	</div>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.0/jquery-ui.min.js"></script>
+	<script src="js/main.js"></script>
+</body>
+</html>
